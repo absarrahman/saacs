af8ebcdf0f746f0a9dacbe27a400e9aa00be2a99
absarrahman <raman040635@gmail.com>
2025-03-21 11:29:40 -0500

    view: update UserCard and AccessControl
    Signed-off-by: absarrahman <raman040635@gmail.com>

---
+++ apps/biochain-web/components/Collection/UserCard.vue
@@ -0,0 +1,151 @@
+<template>
+    <UCard>
+        <Placeholder class="py-10">
+            <div class="flex flex-row justify-between">
+                <div>
+                    <h3>{{ user.name }}</h3>
+                    <p>Role: {{ user.role }}</p>
+                </div>
+                <div flex justify-center items-center>
+                    <UDropdown :items="items" :popper="{ placement: 'bottom-start' }">
+                        <UButton color="white" label="Options" trailing-icon="i-heroicons-chevron-down-20-solid" />
+                    </UDropdown>
+                </div>
+            </div>
+        </Placeholder>
+    </UCard>
+</template>
+
+<!-- <script>
+export default {
+  props: {
+    user: Object
+  },
+  data() {
+    return {
+      items: [
+        [
+          {
+            label: 'Profile',
+            avatar: {
+              src: 'https://avatars.githubusercontent.com/u/739984?v=4'
+            },
+            disabled: true,
+            class: "text-blue-500 opacity-100"
+          },
+          {
+            label: 'Profile',
+            avatar: {
+              src: 'https://avatars.githubusercontent.com/u/739984?v=4'
+            }
+          }
+        ],
+        [
+          {
+            label: 'Edit',
+            icon: 'i-heroicons-pencil-square-20-solid',
+            shortcuts: ['E'],
+            click: () => {
+              console.log('Edit');
+            }
+          },
+          {
+            label: 'Duplicate',
+            icon: 'i-heroicons-document-duplicate-20-solid',
+            shortcuts: ['D'],
+            disabled: true
+          }
+        ],
+        [
+          {
+            label: 'Archive',
+            icon: 'i-heroicons-archive-box-20-solid'
+          },
+          {
+            label: 'Move',
+            icon: 'i-heroicons-arrow-right-circle-20-solid'
+          }
+        ],
+        [
+          {
+            label: 'Delete',
+            icon: 'i-heroicons-trash-20-solid',
+            shortcuts: ['⌘', 'D']
+          }
+        ]
+      ]
+    };
+  }
+};
+</script> -->
+
+<script>
+export default {
+    props: {
+        user: Object
+    },
+    data() {
+        return {
+            items: [
+                [
+                    {
+                        label: 'Edit',
+                        icon: 'i-heroicons-pencil-square-20-solid',
+                        click: () => {
+                            console.log('Edit');
+                        }
+                    },
+
+                ],
+                [
+                    {
+                        label: 'Set role',
+                        disabled: true,
+                        class: "opacity-100",
+                    },
+                    {
+                        label: 'Admin',
+                        class: "font-normal",
+                        click: () => {
+                            console.log('Setting admin');
+                        }
+                    },
+                    {
+                        label: 'Manager',
+                        class: "font-normal",
+                        click: () => {
+                            console.log('Setting role 1');
+                        },
+                    },
+                    {
+                        label: 'Student',
+                        class: "font-normal",
+                        click: () => {
+                            console.log('Setting role 1');
+                        },
+                    },
+                ],
+                [
+                    {
+                        label: 'Delete the user',
+                        icon: 'i-heroicons-trash-20-solid',
+                        click: () => {
+                            console.log('Deleting user');
+                        }
+                    }
+                ]
+            ]
+        };
+    }
+};
+</script>
+
+<style scoped>
+.card {
+    padding: 10px;
+}
+
+.dropdowntext {
+    color: blue;
+}
+</style>
--- apps/biochain-web/pages/collection/[collectionId]/AccessControl.vue
+++ apps/biochain-web/pages/collection/[collectionId]/AccessControl.vue
@@ -1,11 +1,28 @@
 <script lang="ts" setup>
+
 definePageMeta({
   auth: true,
 })
+
+const users = ref([
+  { id: 1, name: 'Alice', role: "Admin" },
+  { id: 2, name: 'Bob', role: "Manager" },
+  { id: 3, name: 'Charlie', role: "Student" }
+]);
+
 </script>
 
 <template>
-  <div>Access Control</div>
+  <h2 class="header">
+    Access Control
+    <CollectionUserCard class="my-10" v-for="user in users" :key="user.id" :user="user" />
+  </h2>
 </template>
 
-<style></style>
+<style scoped>
+.header {
+  font-weight: bold;
+  padding-left: 16px;
+  padding-top: 16px;
+}
+</style>
